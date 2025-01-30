3f28fdbfe0eb4c43130a3eb1403f473315e82ad4
Leonardo Albergaria <leo.albergaria19@gmail.com>
2025-01-30 16:22:10 -0300

    Quick fix

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const { ethers } = require("ethers");
 
-const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const CONTRACT_ADDRESS = "";
 const RPC_URL = "http://127.0.0.1:8545";
 
 const productABI = [
